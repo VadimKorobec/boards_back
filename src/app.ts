3130a02e2bc21fb7a5f84bc9596753a90ff195d1
VadimKorobec <_s_e_t__@ukr.net>
2025-10-18 14:24:56 +0300

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
 import express, { Application, Request, Response } from 'express';
-import { router as userRoutes } from './routes/user.routes';
+import cors from 'cors';
 
 const app: Application = express();
+const PORT: number = 5000;
 
-app.use('/users', userRoutes);
+app.use(cors());
 
-app.use('/', (req: Request, res: Response): void => {
-  res.json({ message: "Miley, what's good?" });
+app.get('/', (req: Request, res: Response): void => {
+  res.send({ message: "Miley, what's good?" });
 });
 
-export default app;
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}...`);
+});

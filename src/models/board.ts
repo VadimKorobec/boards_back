050f89ddf98df571589bbdaf25e6f059755964d7
VadimKorobec <_s_e_t__@ukr.net>
2025-10-22 15:26:09 +0300

    add some code

--- src/models/board.ts
+++ src/models/board.ts
@@ -2,7 +2,7 @@ import { Schema, model } from 'mongoose';
 import taskSchema from './task';
 
 const boardSchema = new Schema({
-  title: String,
+  title: { type: String, required: true },
   toDo: [taskSchema],
   inProgress: [taskSchema],
   done: [taskSchema],
